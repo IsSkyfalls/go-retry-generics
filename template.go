899db86e1b8f58e87b6d57b86211db3ab3b0b1fd
IsSkyfalls <31559463+IsSkyfalls@users.noreply.github.com>
2022-07-17 00:00:34 +0800

    poc: Add backoff to retry

--- template.go
+++ template.go
@@ -6,6 +6,7 @@ type func1to1[TA1 any, TR1 any] func(a1 TA1) (TR1, error)
 
 type retry1to1[TA1 any, TR1 any] struct {
 	f           func1to1[TA1, TR1]
+	backoff     BackoffTimingFunc
 	maxAttempts int
 }
 
@@ -13,6 +14,7 @@ func Try1to1[TA1 any, TR1 any](f func1to1[TA1, TR1]) *retry1to1[TA1, TR1] {
 	return &retry1to1[TA1, TR1]{
 		f:           f,
 		maxAttempts: 0,
+		backoff:     Constant(0),
 	}
 }
 
@@ -21,15 +23,18 @@ func (retry *retry1to1[TA1, TR1]) ForTimes(times int) *retry1to1[TA1, TR1] {
 	return retry
 }
 
+func (retry *retry1to1[TA1, TR1]) WithBackoff(f BackoffTimingFunc) *retry1to1[TA1, TR1] {
+	retry.backoff = f
+	return retry
+}
+
 func (retry retry1to1[TA1, TR1]) Run(a1 TA1) (r1 TR1, err error) {
-	// this allows -1 to be used as infinity (or any negative values)
-	for i := retry.maxAttempts; i != 0; i-- {
+	for i := 0; i < retry.maxAttempts || retry.maxAttempts < 0; i++ {
 		r1, err = retry.f(a1)
 		if err == nil {
 			return r1, nil
 		}
-		//TODO: wait function
-		time.Sleep(time.Second)
+		time.Sleep(retry.backoff(i))
 	}
 	// get uninitialized instance of TR1, aka. null
 	var null TR1
