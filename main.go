781355f81207b3e758c0211ba457944c421ae978
IsSkyfalls <31559463+IsSkyfalls@users.noreply.github.com>
2022-07-17 23:04:25 +0800

    gen: Add generated code

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import _ "github.com/dave/jennifer" // force dep
+import (
+	_ "github.com/dave/jennifer" // force dep
+	. "go-retry-generics/retry"
+)
 
-//go:generate go run codegen.go 1 1
+//go:generate go run codegen.go 8 8
 func main() {
+	// this is for debugging with go build -gcflags "-m"
+	var noOptimize = 0
+	for i := 0; i < 5; i++ {
+		noOptimize, _ = Try1to1(func(x int) (int, error) {
+			return x * 2, nil
+		}).ForTimes(10).Run(i)
+		if noOptimize != i*2 {
+			panic("wrong result")
+		}
+	}
 	panic("Run \"go generate\" to invoke the generator")
 }
